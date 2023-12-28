fc2db3956b7297a7792f07a564a77ab31ee24051
exTerEX <30706568+exTerEX@users.noreply.github.com>
2023-12-28 15:59:52 +0100

    Fix while linking is fixed

--- cfg.ps1
+++ cfg.ps1
@@ -3,6 +3,58 @@
 #Requires -Version 5
 #Requires -RunAsAdministrator
 
+##### While unable to load module
+
+# Hide the path file
+function Hide-File {
+    param([Parameter(Mandatory)][string]$Path)
+
+    if (!(((Get-Item -Path $Path -Force).Attributes.ToString() -Split ", ") -Contains "Hidden")) {
+        (Get-Item -Path $Path -Force).Attributes += "Hidden"
+    }
+}
+
+# Create a new directory in path directory
+function New-Directory {
+    param ([Parameter(Mandatory)][string]$Path, [switch]$Hide)
+
+    PROCESS {
+        If (!(test-path $Path)) {
+            New-Item -Path $Path -ItemType "directory" | Out-Null
+        }
+
+        if ($Hide) { Hide-File($Path) }
+    }
+}
+
+# Create a softlink from path to target
+function Set-Softlink {
+    param ([Parameter(Mandatory)][string]$Path, [Parameter(Mandatory)][string]$Target, [switch]$Hide)
+
+    PROCESS {
+        if (Test-Path -Path $Path) {
+            if (!(Get-Item $Path -Force).LinkType -eq "SymbolicLink") {
+                Write-Host "Old file renamed to $((Get-Item -Path $Path).Name).old..." -ForegroundColor Blue
+                Rename-Item -Path $Path -NewName "$((Get-Item -Path $Path).Name).old"
+
+                Write-Host "Linking: $Target->$Path..." -ForegroundColor Blue
+                New-Item -ItemType SymbolicLink -Path $Path -Target $Target -Force | Out-Null
+            }
+        }
+        else {
+            Write-Host "Linking: $Target->$Path..." -ForegroundColor Blue
+            New-Item -ItemType SymbolicLink -Path $Path -Target $Target -Force | Out-Null
+        }
+
+        if ($Hide) { Hide-File($Path) }
+    }
+}
+
+#####
+
+# Import personal helper files for PowerShell
+#Import-Module -Name "$PSScriptRoot\utils\pwsh-utils\fs-manipulation.ps1" -Verbose
+
 # User-adjusted variables
 [string]$RCONFIG = "$HOME\.config\R"
 [version]$RVERSION = "4.2.2"
@@ -18,9 +70,6 @@ New-Directory -Path "$RCONFIG"
 New-Directory -Path "$(Join-Path -Path "$RCONFIG\lib" -ChildPath $USERVERSION)"
 
 Write-Host "Configuring System..." -ForegroundColor "Green"
-
-# Import personal helper files for PowerShell
-Import-Module -Name "$PSScriptRoot\utils\pwsh-utils\fs-manipulation.ps1" -Verbose
 
 # Install R for windows
 winget install Rproject.R --scope machine --architecture x64 --location $RPATH --version $RVERSION # TODO: No install icons
